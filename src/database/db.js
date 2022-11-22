87cadff966635fbbe1a16f1551840068d8a5222f
ConstantinoRafael <rafael.constantino89@gmail.com>
2022-11-22 16:13:05 -0300

    feat: database

---
+++ src/database/db.js
@@ -0,0 +1,16 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+try {
+  await mongoClient.connect();
+  console.log("MongoDB connected!");
+} catch (err) {
+  console.log(err);
+}
+
+const db = mongoClient.db("livre-se");
+export const usersCollection = db.collection("users");
+export const sessionsCollection = db.collection("sessions");
