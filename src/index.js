e2944af5d9164240220c06053cfc808ed439cba1
ConstantinoRafael <rafael.constantino89@gmail.com>
2022-11-23 03:00:43 -0300

    feat: config to atlas mongodb

--- src/index.js
+++ src/index.js
@@ -3,10 +3,44 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
+//
+import { MongoClient } from "mongodb";
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+//Para testar a conexão com o banco de dados
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+try {
+  await mongoClient.connect();
+  console.log("MongoDB connected!");
+} catch (err) {
+  console.log(err);
+}
+
+const db = mongoClient.db("livre-se");
+
+app.post("/teste", async (req, res) => {
+  try {
+    const testeBody = req.body;
+    await db.collection("testebanco").insertOne(testeBody);
+    res.sendStatus(201);
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
+
+app.get("/teste", async (req, res) => {
+  try {
+    const testes = await db.collection("teste-banco").find().toArray();
+    res.send(testes);
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
 
 const port = process.env.PORT || 5000;
 
