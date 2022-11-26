40af7666381342d35206437aecbd7ec5f48df040
DanielIbiapina <daniel18.ibiapinabarros@gmail.com>
2022-11-26 18:55:38 -0300

    fix: rota carrinho

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 import userRoutes from "./routes/user.routes.js"
 import bookRoutes from "./routes/book.routes.js"
+import carrinhoRoutes from "./routes/carrinho.routes.js"
 
 
 const app = express();
@@ -12,7 +13,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(userRoutes);
-app.use(bookRoutes)
+app.use(bookRoutes);
+app.use(carrinhoRoutes);
 
 
 
